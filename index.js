14b437a9f1df15e33c73bcff8bad94a86424f7b4
Casey D <caseydasilva@hotmail.com>
2020-09-25 07:18:07 -0700

    Grid and cell structure
    added new dynamic structure for grid and cell creation
    added ability to randomly choose which 'walls' of the cells to eliminate in order to create random mazes that will always have 1 correct answer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+//square maze prefered for ease in auto-creation
+const width = 600;
 const height = 600;
+const cells = 3;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -9,7 +11,7 @@ const render = Render.create({
 	element : document.body,
 	engine  : engine,
 	options : {
-		wireframes : false,
+		wireframes : true,
 		width,
 		height
 	}
@@ -17,27 +19,74 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-	world,
-	MouseConstraint.create(engine, {
-		mouse : Mouse.create(render.canvas)
-	})
-);
-
 //Walls
 const walls = [
-	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-	Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-	Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
 
-//Random Shapes
-for (let i = 0; i < 50; i++) {
-	if (Math.random() > 0.5) {
-		World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-	} else {
-		World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35));
+//maze generation
+const shuffle = (arr) => {
+	let counter = arr.length;
+	while (counter > 0) {
+		const index = Math.floor(Math.random() * counter);
+		counter--;
+		const temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
 	}
-}
+	return arr;
+};
+
+// the first Array(3) determines how many rows; the 2nd determines the columns (this allows them to change independently T/F)
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+	//If cell already visited, return
+	if (grid[row][column]) {
+		return;
+	}
+	//otherwise mark cell as visited (update cell to 'true')
+	grid[row][column] = true;
+	//Assemble randomly ordered list of neighboring cells
+	const neighbors = shuffle([
+		[ row - 1, column, 'up' ],
+		[ row, column + 1, 'right' ],
+		[ row + 1, column, 'down' ],
+		[ row, column - 1, 'left' ]
+	]);
+
+	//for each neighbor cell:
+	for (let neighbor of neighbors) {
+		const [ nextRow, nextColumn, direction ] = neighbor;
+		//see if neighbor is out of bounds
+		if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+			continue;
+		}
+		//if visited, ctn to next neighbor
+		if (grid[nextRow][nextColumn]) {
+			continue;
+		}
+		//remove wall from either horizontals or verticals array
+		if (direction === 'left') {
+			verticals[row][column - 1] = true;
+		} else if (direction === 'right') {
+			verticals[row][column] = true;
+		} else if (direction === 'up') {
+			horizontals[row - 1][column] = true;
+		} else if (direction === 'down') {
+			horizontals[row][column] = true;
+		}
+	}
+	//visit chosen cell (call stepThroughCell func again)
+};
+
+stepThroughCell(startRow, startColumn);
